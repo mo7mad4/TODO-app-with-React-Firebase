15a2f944ace15c693c2557151fed49fbd95ae942
Mohammed Zeyad Abu Raida <mohammedreda814@gmail.com>
2022-04-08 15:51:31 +0300

    Push my code in github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState(['Take dogs for walk', 'Take the rubbish out', 'qazi wants to livestream today']);
+  const [input, setInput] = useState('');
+  console.log('❤', input);
+
+  const addTodo = (event) => {
+    //this will fire off  when we click the button
+    event.preventDefault(); //will stop the REFRESH
+    console.log("💨", "I'm working!!!");
+    setTodos([...todos, input]);
+    setInput(''); //clear up the input after hitting clicking add todo button
+    
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Todo App 🤍</h1>
+
+     <form>
+        <input value={input} onChange={event => setInput(event.target.value)}/>
+        <button onClick={addTodo}>Add todo</button>
+     </form>
+
+      <ul>
+        {/* زي وكأنو في مصفوفة وعامل عليها فور لووب */}
+        {todos.map(todo => (
+          <li>{todo}</li>
+        )) }
+      </ul>
+
     </div>
   );
 }
